c3ca48f4271ffaa9c211700538054767a8d02364
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-23 20:31:13 +0530

    ADDED: all the required options for the game added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,11 +2,37 @@ package com.bridgelabz;
 
 public class SnakeAndLadder
 {
+    public static final int NO_PLAY = 1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
+    public static final int START_POSITION = 0;
     public static void main(String[] args)
     {
-        int START_POSITION = 0;
-        System.out.println(START_POSITION);
-        int ROLL_DICE = (int)(Math.floor(Math.random() * 10) % 6) + 1;
-        System.out.println("After the rolling dice we get :: " +ROLL_DICE);
+        int playerPosition = 0;
+        int ROLL_DICE = (int) (Math.random() * 6 );
+        System.out.println("After Rolling Dice We Get ::"+ROLL_DICE);
+        int playerCheckOption;
+        playerCheckOption = (int)(Math.random() *3 ) + 1;
+
+        switch(playerCheckOption)
+        {
+            case NO_PLAY:
+                System.out.println("It's No Play, Your position is not changed, You are at :" + playerPosition);
+                break;
+
+            case LADDER:
+                playerPosition = playerPosition + ROLL_DICE;
+                System.out.println("Congratulations !! You Got the ladder your position will be increased by " + ROLL_DICE);
+                break;
+
+            case SNAKE:
+                playerPosition = playerPosition - ROLL_DICE;
+                System.out.println("Oops !! You just got bitten by snake your position will be increased by " + ROLL_DICE);
+                break;
+
+            default:
+                System.out.println("Something went wrong!!");
+        }
+
     }
 }
