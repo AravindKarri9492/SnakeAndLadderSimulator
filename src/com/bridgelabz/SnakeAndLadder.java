bc42fabe7222085b1dc967345c5fe1feb441c819
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-23 20:03:28 +0530

    ADDED: code to get numbers b/w 1 to 6 after the rolling dice

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,5 +6,7 @@ public class SnakeAndLadder
     {
         int START_POSITION = 0;
         System.out.println(START_POSITION);
+        int ROLL_DICE = (int)(Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println("After the rolling dice we get :: " +ROLL_DICE);
     }
 }
